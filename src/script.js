90dcc2d3f11ba636392d249445b1c2b3646b8d33
Xtracly <93444147+Xtracly@users.noreply.github.com>
2023-06-26 08:37:31 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -8,21 +8,3 @@ const swiper = new Swiper(".swiper", {
         disableOnInteraction: false,
     },
 });
-
-// Анімація затримки відправлення форми
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    // Затримка перед відправленням форми
-    setTimeout(function() {
-      // Ваш код для відправлення форми
-      // Наприклад, ви можете використовувати AJAX або відправити дані на сервер
-      // Після відправлення форми очистити поля введення
-      document.getElementById("name").value = "";
-      document.getElementById("phone").value = "";
-      document.getElementById("message").value = "";
-      // Відображення підтвердження або повідомлення про помилку
-      // Можна вивести повідомлення на сторінку або використовувати модальне вікно
-      // Наприклад:
-      alert("Дякуємо! Ваше повідомлення надіслано.");
-    }, 1000);
-  });
